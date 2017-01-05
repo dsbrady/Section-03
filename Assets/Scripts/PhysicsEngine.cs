04d60c110aabe326554016adb43aa15484c4bbd6
Scott Brady <sbrady@globalcloud.net>
2017-01-05 09:41:11 -0600

    Finished course

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+
+	public Vector3 initialAngularVelocity = new Vector3(4f, 0, 0);
+
+	private Rigidbody rigidBody;
+
+	void OnEnable () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.angularVelocity = this.initialAngularVelocity;
+	}
+}
---
+++ Assets/Scripts/Torque.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torque : MonoBehaviour {
+
+	public Vector3 torque = new Vector3(0, 0, 0);
+	public float torqueTime = 0f;
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		if (this.torqueTime >= 0f) {
+			rigidBody.AddTorque(this.torque);
+			this.torqueTime -= Time.deltaTime;
+		}
+	}
+}
